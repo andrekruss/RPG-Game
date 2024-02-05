1da820e93a15eaa40d71594c144237378fbd8dfc
Andre Kruss <andrebeltramekruss@gmail.com>
2024-02-05 11:02:01 -0300

    Basic tests with projectiles
    Testing projectiles with basic rectangle shapes, calculating position vectors and diretion vectors for projectiles

--- RPG-Game/main.cpp
+++ RPG-Game/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+sf::Vector2f normalizeVector(sf::Vector2f vector);
+
 int main()
 {
     //-------------------------------INITIALIZATION-------------------------------//
@@ -12,10 +14,12 @@ int main()
     //-------------------------------INITIALIZATION-------------------------------//
 
     //-------------------------------LOAD-------------------------------//
-    sf::Texture skeletonTexture;
-    sf::Sprite skeletonSprite;
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
+    sf::Texture skeletonTexture;
+    sf::Sprite skeletonSprite;
+    std::vector<sf::RectangleShape> bullets;
+    //sf::RectangleShape bullet(sf::Vector2f(50.0f, 25.0f));
     int xPlayerSpriteIndex = 4, yPlayerSpriteIndex = 2;
     int xSkeletonSpriteIndex = 2, ySkeletonSpriteIndex = 3;
     int spriteWidth = 64, spriteHeight = 64;
@@ -25,10 +29,8 @@ int main()
     {
         std::cout << "Skeleton texture succesfully loaded!" << std::endl;
         skeletonSprite.setTexture(skeletonTexture);
-
         skeletonSprite.setTextureRect(sf::IntRect(64 * xSkeletonSpriteIndex, 64 * ySkeletonSpriteIndex, spriteWidth, spriteHeight));
         skeletonSprite.scale(sf::Vector2f(3.0f, 3.0f));
-        skeletonSprite.setPosition(sf::Vector2f(400.0f, 400.0f));
     }
 
     else
@@ -40,7 +42,6 @@ int main()
     {
         std::cout << "Player texture succesfully loaded!" << std::endl;
         playerSprite.setTexture(playerTexture);
-
         playerSprite.setTextureRect(sf::IntRect(64 * xPlayerSpriteIndex, 64 * yPlayerSpriteIndex, spriteWidth, spriteHeight));
         playerSprite.scale(sf::Vector2f(3.0f, 3.0f));
     }
@@ -49,7 +50,12 @@ int main()
         std::cout << "Player image failed to load!" << std::endl;
     }
     //-------------------------------LOAD-------------------------------//
-
+    playerSprite.setPosition(sf::Vector2f(windowWidth / 2.0, windowHeight / 2.0));
+    //skeletonSprite.setPosition(sf::Vector2f(400.0f, 400.0f));
+    //bullet.setPosition(playerSprite.getPosition());
+  
+    sf::Vector2f enemyPosition;
+    sf::Vector2f normalizedBulletDirection;
     // Game Loop
     while (window.isOpen())
     {
@@ -73,6 +79,22 @@ int main()
 
             else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
                 playerSprite.setPosition(previousPosition + sf::Vector2f(0, -5));         
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+            {
+                bullets.push_back(sf::RectangleShape(sf::Vector2f(50.0f, 25.0f)));
+                int lastBulletIndex = bullets.size() - 1;
+                bullets[lastBulletIndex].setPosition(playerSprite.getPosition());
+            }
+        }
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            sf::Vector2f bulletDirection;
+            enemyPosition = skeletonSprite.getPosition();
+            bulletDirection = enemyPosition - bullets[i].getPosition();
+            normalizedBulletDirection = normalizeVector(bulletDirection);
+            bullets[i].setPosition(bullets[i].getPosition() + normalizedBulletDirection);
         }
 
          //-------------------------------UPDATE-------------------------------//
@@ -81,9 +103,19 @@ int main()
             window.clear(sf::Color::Black);
             window.draw(playerSprite);
             window.draw(skeletonSprite);
+            for (size_t i = 0; i < bullets.size(); i++)
+            {
+                window.draw(bullets[i]);
+            }
             window.display();
             //-------------------------------DRAW-------------------------------//
     }
 
     return 0;
+}
+
+sf::Vector2f normalizeVector(sf::Vector2f vector) {
+    float magnitude = sqrt(vector.x * vector.x + vector.y * vector.y);
+    float speed = 0.1f; // using to make the projectile a bit slower
+    return sf::Vector2f(speed * vector.x / magnitude, speed * vector.y / magnitude);
 }
